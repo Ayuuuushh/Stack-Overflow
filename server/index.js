d5dbcfa746e6e48b2e198cc9d507a05966dfef64
Ayush <ayushkk9893@gmail.com>
2023-06-30 17:11:52 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -5,10 +5,9 @@ import dotenv from "dotenv";
 
 import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
-import answerRoutes from "./routes/Answers.js";
+import answerRoutes from "./routes/Answer.js";
 
 dotenv.config();
-connectDB();
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
